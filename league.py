ddb054c9db684ec1dd30be0f694768674f7069c6
sportzwiz321 <nmchen@ucsc.edu>
2017-09-30 06:54:22 -1000

    API Update plus Key Removal
    Program updated to utilize the new API as well as remove using the key
    in every single request. Key must be inputted to use.

--- league.py
+++ league.py
@@ -1,17 +1,22 @@
 import requests
+import sys
+
+# update key information here
+# key = 
 
 def retrieveSummoner(name):
-	URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + NAME + "?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff"
+	URL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + NAME + "?api_key=" + key
 	me = requests.get(URL).json()
-	USERNAME = me[NAME]['name']
-	LEVEL = me[NAME]['summonerLevel']
-	ID = me[NAME]['id']
+
+	USERNAME = me['name']
+	LEVEL = me['summonerLevel']
+	ID = me['id']
 
 	print "Username:", USERNAME
 	print "Level:", LEVEL
 	print "Identification Code:", ID
 
-	URL3 = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + str(ID) + "/entry?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff"
+	URL3 = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/" + str(ID) + "/?api_key=" + key
 	rank = requests.get(URL3).json()
 
 	ID = str(ID)
@@ -21,16 +26,33 @@ def retrieveSummoner(name):
 	# if len(rank[ID] != 1):
 	# 	return
 
+	# print rank
 	# print len(rank)
 
-	if rank.has_key(ID) is True:
-		tier = rank[ID][0]['tier']
-		division = rank[ID][0]['entries'][0]['division']
-		leaguePoints = rank[ID][0]['entries'][0]['leaguePoints']
+	# if rank.has_key(ID) is True:
+	if len(rank) > 0:
+		for x in range(0,len(rank)):
+			# print x
+			print
+			data = rank[x]
+			queueType = data['queueType']
+			tier = data['tier']
+			division = data['rank']
+			leaguePoints = data['leaguePoints']
 
-		print "Tier:", tier
-		print "Division:", division
-		print "LP:", leaguePoints
+			print "Queue Type:", queueType
+			print "Tier:", tier
+			print "Division:", division
+			print "LP:", leaguePoints
+
+		# tier = rank[ID][0]['tier']
+		# division = rank[ID][0]['entries'][0]['division']
+		# leaguePoints = rank[ID][0]['entries'][0]['leaguePoints']
+
+		# print "Ranked Solo/Duo"
+		# print "Tier:", tier
+		# print "Division:", division
+		# print "LP:", leaguePoints
 	else:
 		print "You have not played enough ranked games to be placed yet"
 		print "You suck."
@@ -50,21 +72,27 @@ def retrieveSummoner(name):
 
 	return ID
 
-def retrieveChampion(list, count):
+def retrieveChampion(list, order, record, recordPoints, count):
 
 	if len(list) == 0:
-		print "Sorry you haven't earned any mastery points on any champions yet"
+		# print "Sorry you haven't earned any mastery points on any champions yet"
 		return
 	elif count > len(list):
-		print "Sorry you haven't played", count, "champions yet"
-		print "You have only earned mastery for", len(list), "champions"
+		# print "Sorry you haven't played", count, "champions yet"
+		# print "You have only earned mastery for", len(list), "champions"
 		count = len(list)
-	print "Your top", count, "champions are:"
+
+	print str(count) + "/138 played"
+	print
+	print "Your top champions are:"
 
 	x = 0
 
 	masteries=[]
-	mIndex = []
+	# mIndex = []
+	for i in range(1,9):
+		record[i] = 0
+		recordPoints[i] = 0
 
 	while x < count:
 		placed = False
@@ -73,26 +101,33 @@ def retrieveChampion(list, count):
 			# print "My index is", index
 			# if len(masteries) > 0:
 				# print "Top champ is", masteries[0]
+			masteryLevel = list[x]['championLevel']
 			if len(masteries) < index + 1:
 				masteries.append(list[x]['championId'])
-				mIndex.append(x)
+				order.append(x)
+				record[masteryLevel] += 1
+				recordPoints[masteryLevel] += list[x]['championPoints']
+				record[8] += masteryLevel
 				# print "Champion", x, "is placed here"
 				placed = True
-			elif list[x]['championLevel'] > list[mIndex[index]]['championLevel'] or list[x]['championPoints'] > list[mIndex[index]]['championPoints']:
+			elif masteryLevel > list[order[index]]['championLevel'] or list[x]['championPoints'] > list[order[index]]['championPoints']:
 				# print "Champion", x, "Mastery Level:", list[x]['championLevel']
 				# print "Index", index, "Champion Mastery Level:", list[index]['championLevel']
 				# print "Champion", x, "Champion Mastery Level at index", index, "is", list[x]['championLevel'] > list[index]['championLevel']
 				# print "Champion", x, "Champion Mastery Points at index", index, "is", list[x]['championPoints'] > list[index]['championPoints']
 				dec = len(masteries)
 				masteries.append(masteries[dec - 1])
-				mIndex.append(mIndex[dec - 1])
+				order.append(order[dec - 1])
 				dec -= 1
 				while dec > index:
 					masteries[dec] = masteries[dec - 1]
-					mIndex[dec] = mIndex[dec - 1]
+					order[dec] = order[dec - 1]
 					dec -= 1
 				masteries[index] = list[x]['championId']
-				mIndex[index] = x
+				order[index] = x
+				record[masteryLevel] += 1
+				record[8] += masteryLevel
+				recordPoints[masteryLevel] += list[x]['championPoints']
 				# while index < len(masteries):
 				# 	tempChamp = masteries[index]
 				# 	masteries[index] = list[x]['championId']
@@ -112,42 +147,107 @@ def retrieveChampion(list, count):
 		# 	c += 1
 
 
+	# list = masteries
+
 	x = 0
-	while x < count:
-		id = masteries[x]
-		# id = list[x]['championId']
-		champion = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(id) + "?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff")
-		# champ = champion.json()
-		print champion.json()['name'], "Mastery Lvl:", list[mIndex[x]]['championLevel'], "Champion Pts:", list[mIndex[x]]['championPoints']
-		# print x+1
-		x += 1
+	# while x < count:
+	# 	id = masteries[x]
+	# 	# id = list[x]['championId']
+	# 	# list[x]['championId'] is equivalent to masteries[x]
+
+	# 	champion = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(id) + "?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff")
+	# 	# champ = champion.json()
+	# 	# print champion.json()['name'], "Mastery Lvl:", list[mIndex[x]]['championLevel'], "Champion Pts:", list[mIndex[x]]['championPoints']
+	# 	# print x+1
+	# 	x += 1
 	# print "all done!"
 	
+	# order = mIndex
+
 	# return champion.json()['name']
+
+def printTopChampions(list, order, record, recordPoints):
+	champList = requests.get("https://na1.api.riotgames.com/lol/static-data/v3/champions?dataById=true&api_key=" + key).json()
+	# print champList
+	lowestMastery = 4
+	if len(list) == 0:
+		print "You have no mastery"
+		print "Play some more games before coming back"
+	else:
+		print "Mastery Score:", record[8]
+		x = 7
+		i = 0
+		print "Mastery", x, "(" + (str)(record[x]) + ")"
+		while x >= lowestMastery and i < len(order):
+			# print i
+			# id = list[i]['championId']
+			# champion = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(id) + "?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff")
+			# print champion.json()['name'], x
+			if list[order[i]]['championLevel'] == x:
+				# print "yay"
+				id = list[order[i]]['championId']
+				# champion = requests.get("https://na1.api.riotgames.com/lol/static-data/v3/champions/" + str(id) + "?api_key=c07d4fd5-82c1-4816-a45c-605b8291daff")
+				# print id
+				# if champion.json().has_key('name'):
+				print "\t", champList['data'][str(id)]['name'], list[order[i]]['championPoints']
+				i += 1
+			else:
+				x -= 1
+				if x >= lowestMastery:
+					print
+					print "Mastery", x, "(" + (str)(record[x]) + ")"
 
 # def main():
 
-NAME = (str)(raw_input("What is your summoners name?\n"))
-NUMBER = (int)(raw_input("How many of your top champions would you like to view?\n"))
-# print str(NUMBER)
+if not len(sys.argv) == 2:
+	print "RuntimeError: Username is required"
+else:
 
-# NAME = "ktdesk"
+	# print len(sys.argv)
 
-id = retrieveSummoner(NAME)
+	NAME = sys.argv[1]
 
-# print me[NAME]
+	URL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + NAME + "?api_key=" + key
+	me = requests.get(URL).json()
+
+	print me
+
+	if not me.has_key('name'):
+		print "Summoner:", NAME, "does not exist"
+		print "Please try again"
+	else:	
+
+		print sys.argv[1]
+
+		id = retrieveSummoner(NAME)
+
+		URL2 = "https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + id + "?api_key=" + key
+		mastery = requests.get(URL2).json()
+
+		# print "Mastery Length:", len(mastery)
+
+		order = []
+		record = {}
+		recordPoints = {}
+
+		count = 138
+
+		retrieveChampion(mastery, order, record, recordPoints, count)
+
+		printTopChampions(mastery, order, record, recordPoints)
+
+	# NAME = (str)(raw_input("What is your summoners name?\n"))
+	# NUMBER = (int)(raw_input("How many of your top champions would you like to view?\n"))
+
+	
 
 
-# NUMBER = 5
-URL2 = "https://na.api.pvp.net/championmastery/location/NA1/player/" + id + "/topchampions?count=" + str(NUMBER) + "&api_key=c07d4fd5-82c1-4816-a45c-605b8291daff"
-mastery = requests.get(URL2).json()
 
-# CHAMPIONS = [mastery[0][championId], mastery[1][championId], mastery[2][championId], []]
 
-retrieveChampion(mastery, NUMBER)
-# print mastery[0]
-# print retrieveChampion(mastery[0]['championId'])
-# print retrieveChampion(mastery[1]['championId'])
-# print retrieveChampion(mastery[2]['championId'])
-# print retrieveChampion(mastery[3]['championId'])
-# print retrieveChampion(mastery[4]['championId'])
+
+
+
+
+
+
+
